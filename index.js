f43fc5b51239a28e7cd45afff73e4fd04681af33
torobucci <kevintoro254@gmail.com>
2023-02-20 23:40:55 +0300

    Make changes required

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-let bookCollection = localStorage.getItem('bookCollection')
+const bookCollection = localStorage.getItem('bookCollection')
   ? JSON.parse(localStorage.getItem('bookCollection'))
   : [];
 const title = document.querySelector('#title-input');
 const author = document.querySelector('#author-input');
 const bookContainer = document.querySelector('.books-container');
-
-const removeBook = function () {
-  const title = this.id.split('-')[0];
-  const author = this.id.split('-')[1];
-  bookCollection = bookCollection.filter(
-    (book) => book.title !== title && book.author !== author,
-  );
-
-  displayBooks(); // eslint-disable-line no-use-before-define
-  localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-};
 
 function displayBooks() {
   bookContainer.innerHTML = '';
@@ -29,25 +18,28 @@ function displayBooks() {
     </div>`,
     );
   });
-  document.querySelectorAll('.remove').forEach((removeBtn) => {
-    removeBtn.addEventListener('click', removeBook);
+
+  const addButton = document.querySelector('.add-button');
+  addButton.addEventListener('click', () => {
+    if (title.value && author.value) {
+      const bookObject = {
+        title: title.value,
+        author: author.value,
+      };
+      bookCollection.push(bookObject);
+      title.value = '';
+      author.value = '';
+      displayBooks();
+      localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    }
+  });
+  const removeBtns = document.querySelectorAll('.remove');
+  removeBtns.forEach((removeBtn, i) => {
+    removeBtn.addEventListener('click', () => {
+      bookCollection.splice(i, 1);
+      displayBooks();
+      localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    });
   });
 }
-
-const addBook = function () {
-  if (title.value && author.value) {
-    const bookObject = {
-      title: title.value,
-      author: author.value,
-    };
-    bookCollection.push(bookObject);
-    title.value = '';
-    author.value = '';
-    displayBooks();
-    localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-  }
-};
-
-const addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', addBook);
 displayBooks();
