65fbbabe6fa5680126c6e056de8c8c4100b35db0
torobucci <kevintoro254@gmail.com>
2023-02-20 12:01:19 +0300

    display div when bookObject added

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const bookCollection = [];
 
-const title = document.querySelector("#title-input");
-const author = document.querySelector("#author-input");
+const title = document.querySelector('#title-input');
+const author = document.querySelector('#author-input');
+const bookContainer = document.querySelector('.book-container');
 
-const addBook = function () {
+const addBook = function() {
   const bookObject = {
     title: title.value,
     author: author.value,
   };
   bookCollection.push(bookObject);
-  title.value = "";
-  author.value = "";
+  title.value = '';
+  author.value = '';
+
+  bookContainer.innerHTML = '';
+  bookContainer.insertAdjacentHTML('beforeend', `<div>
+   <p>${bookObject.title}</p>
+   <p>${bookObject.author}</p>
+    <button class="remove" type="button">Remove</button>
+    <hr>
+    </div>`);
 };
 
-const addButton = document.querySelector(".add-button");
-addButton.addEventListener("click", addBook);
+const addButton = document.querySelector('.add-button');
+addButton.addEventListener('click', addBook);
