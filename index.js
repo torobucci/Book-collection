da2bac96c61a3c6bbd3ea13f4786ce31c13984b8
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-20 10:32:26 +0200

    Create addBook function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const bookCollection =[]
+const bookCollection = [];
+
+const title = document.querySelector("#title-input");
+const author = document.querySelector("#author-input");
+
+const addBook = function () {
+  const bookObject = {
+    title: title.value,
+    author: author.value,
+  };
+  bookCollection.push(bookObject);
+  title.value = "";
+  author.value = "";
+};
+
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", addBook);
