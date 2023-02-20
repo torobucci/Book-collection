6f9f737498c0478ccedc277e5ffd9dbc8798843b
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-20 11:35:32 +0200

    Add remove book function

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
-const bookCollection = [];
+let bookCollection = [];
 
-const title = document.querySelector('#title-input');
-const author = document.querySelector('#author-input');
-const bookContainer = document.querySelector('.book-container');
+const title = document.querySelector("#title-input");
+const author = document.querySelector("#author-input");
+const bookContainer = document.querySelector(".books-container");
 
-const addBook = function() {
-  const bookObject = {
-    title: title.value,
-    author: author.value,
-  };
-  bookCollection.push(bookObject);
-  title.value = '';
-  author.value = '';
-
-  bookContainer.innerHTML = '';
-  bookContainer.insertAdjacentHTML('beforeend', `<div>
-   <p>${bookObject.title}</p>
-   <p>${bookObject.author}</p>
-    <button class="remove" type="button">Remove</button>
-    <hr>
-    </div>`);
+const removeBook = function () {
+  const title = this.id.split("-")[0];
+  const author = this.id.split("-")[1];
+  bookCollection = bookCollection.filter((book) => {
+    book.title !== title;
+    book.author !== author;
+  });
+  displayBooks();
 };
 
-const addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', addBook);
+function displayBooks() {
+  bookContainer.innerHTML = "";
+  bookCollection.forEach((book) => {
+    bookContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div>
+   <p>${book.title}</p>
+   <p>${book.author}</p>
+    <button class="remove" type="button" id="${book.title}-${book.author}">Remove</button>
+    <hr>
+    </div>`
+    );
+  });
+  document.querySelectorAll(".remove").forEach((removeBtn) => {
+    removeBtn.addEventListener("click", removeBook);
+  });
+}
+
+const addBook = function () {
+  if (title.value && author.value) {
+    const bookObject = {
+      title: title.value,
+      author: author.value,
+    };
+    bookCollection.push(bookObject);
+    title.value = "";
+    author.value = "";
+    displayBooks();
+  }
+};
+
+const addButton = document.querySelector(".add-button");
+addButton.addEventListener("click", addBook);
