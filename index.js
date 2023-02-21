d84e8aa8a2b627ebaa35623213034e4d9c273f86
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-21 15:48:03 +0200

    Use of classes

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
-const bookCollection = localStorage.getItem('bookCollection')
-  ? JSON.parse(localStorage.getItem('bookCollection'))
-  : [];
-const title = document.querySelector('#title-input');
-const author = document.querySelector('#author-input');
-const bookContainer = document.querySelector('.books-container');
+const title = document.querySelector("#title-input");
+const author = document.querySelector("#author-input");
+const bookContainer = document.querySelector(".books-container");
 
-function displayBooks() {
-  bookContainer.innerHTML = '';
-  bookCollection.forEach((book) => {
-    bookContainer.insertAdjacentHTML(
-      'beforeend',
-      `<div>
+class BookCollectionClass {
+  constructor(bookArray) {
+    this.bookArray = bookArray;
+  }
+  get addBook() {
+    if (title.value && author.value) {
+      this.bookArray.push(new Book(title.value, author.value));
+      console.log(this.bookArray[0]);
+      this.bookArray[0].displayBooks();
+      localStorage.setItem(
+        "bookCollectionArray",
+        JSON.stringify(this.bookArray)
+      );
+    }
+  }
+  removeBook(bk) {
+    this.bookArray.splice(bk, 1);
+    localStorage.setItem("bookCollectionArray", JSON.stringify(this.bookArray));
+  }
+}
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+  displayBooks() {
+    bookContainer.innerHTML = "";
+    bookCollection.bookArray.forEach((book) => {
+      bookContainer.insertAdjacentHTML(
+        "beforeend",
+        `<div>
    <p>${book.title}</p>
    <p>${book.author}</p>
-    <button class="remove" type="button" id="${book.title}-${book.author}">Remove</button>
+    <button class="remove" type="button">Remove</button>
     <hr>
-    </div>`,
-    );
-  });
-
-  const addButton = document.querySelector('.add-button');
-  addButton.addEventListener('click', () => {
-    if (title.value && author.value) {
-      const bookObject = {
-        title: title.value,
-        author: author.value,
-      };
-      bookCollection.push(bookObject);
-      title.value = '';
-      author.value = '';
-      displayBooks();
-      localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
-    }
-  });
-  const removeBtns = document.querySelectorAll('.remove');
-  removeBtns.forEach((removeBtn, i) => {
-    removeBtn.addEventListener('click', () => {
-      bookCollection.splice(i, 1);
-      displayBooks();
-      localStorage.setItem('bookCollection', JSON.stringify(bookCollection));
+    </div>`
+      );
     });
-  });
+    title.value = "";
+    author.value = "";
+    const removeBtns = document.querySelectorAll(".remove");
+    removeBtns.forEach((removeBtn, i) => {
+      removeBtn.addEventListener("click", () => {
+        bookCollection.removeBook(i);
+        this.displayBooks();
+      });
+    });
+  }
 }
-displayBooks();
+const addButton = document.querySelector(".add-button");
+console.log(addButton);
+addButton.addEventListener("click", () => {
+  bookCollection.addBook;
+});
+
+let bookCollection = new BookCollectionClass([]);
+if (localStorage.getItem("bookCollectionArray")) {
+  JSON.parse(localStorage.getItem("bookCollectionArray")).forEach((book) =>
+    bookCollection.bookArray.push(new Book(book.title, book.author))
+  );
+}
+bookCollection.bookArray[0].displayBooks();
