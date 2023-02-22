e32ecc502ac56725fb4edd0080159bb37f6a5236
torobucci <kevintoro254@gmail.com>
2023-02-22 13:18:42 +0300

    fix linter error

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const bookContainer = document.querySelector(".books-container");
+const bookContainer = document.querySelector('.books-container');
 
 function displayBook(book) {
   bookContainer.insertAdjacentHTML(
-    "beforeend",
+    'beforeend',
     `<div class="book-details" id="book-${book.id}">
         <p>"${book.title}" by ${book.author}</p> 
         <button class="remove" type="button" id="remove-${book.id}">Remove</button>
-      </div>`
+      </div>`,
   );
 }
 
@@ -17,20 +17,20 @@ class BookCollection {
 
   removeBook(book) {
     this.bookArray.splice(book, 1);
-    localStorage.setItem("bookCollectionArray", JSON.stringify(this.bookArray));
-    bookContainer.innerHTML = "";
+    localStorage.setItem('bookCollectionArray', JSON.stringify(this.bookArray));
+    bookContainer.innerHTML = '';
     this.bookArray.forEach((book, i) => {
       book.id = i;
       displayBook(book);
       if (document.querySelector(`#remove-${book.id}`)) {
         document
           .querySelector(`#remove-${book.id}`)
-          .addEventListener("click", this.removeBook.bind(this, book));
+          .addEventListener('click', this.removeBook.bind(this, book));
       }
     });
 
-    if (this.bookArray[0]) bookContainer.style.border = "solid 3px #000000";
-    else bookContainer.style.border = "none";
+    if (this.bookArray[0]) bookContainer.style.border = 'solid 3px #000000';
+    else bookContainer.style.border = 'none';
   }
 
   addBook(title, author) {
@@ -39,29 +39,29 @@ class BookCollection {
       author,
       id: this.bookArray.length,
     });
-    localStorage.setItem("bookCollectionArray", JSON.stringify(this.bookArray));
+    localStorage.setItem('bookCollectionArray', JSON.stringify(this.bookArray));
 
     displayBook(this.bookArray[this.bookArray.length - 1]);
     if (
       document.querySelector(
-        `#remove-${this.bookArray[this.bookArray.length - 1].id}`
+        `#remove-${this.bookArray[this.bookArray.length - 1].id}`,
       )
     ) {
       document
         .querySelector(
-          `#remove-${this.bookArray[this.bookArray.length - 1].id}`
+          `#remove-${this.bookArray[this.bookArray.length - 1].id}`,
         )
-        .addEventListener("click", () => {
+        .addEventListener('click', () => {
           this.removeBook(this.bookArray[this.bookArray.length - 1]);
         });
     }
-    if (this.bookArray[0]) bookContainer.style.border = "solid 3px #000000";
-    else bookContainer.style.border = "none";
+    if (this.bookArray[0]) bookContainer.style.border = 'solid 3px #000000';
+    else bookContainer.style.border = 'none';
   }
 }
 
-const bookCollection = localStorage.getItem("bookCollectionArray")
-  ? new BookCollection(JSON.parse(localStorage.getItem("bookCollectionArray")))
+const bookCollection = localStorage.getItem('bookCollectionArray')
+  ? new BookCollection(JSON.parse(localStorage.getItem('bookCollectionArray')))
   : new BookCollection([]);
 bookCollection.bookArray.forEach((book) => {
   displayBook(book);
@@ -69,22 +69,20 @@ bookCollection.bookArray.forEach((book) => {
     document
       .querySelector(`#remove-${book.id}`)
       .addEventListener(
-        "click",
-        bookCollection.removeBook.bind(bookCollection, book)
+        'click',
+        bookCollection.removeBook.bind(bookCollection, book),
       );
   }
 });
-if (bookCollection.bookArray[0])
-  bookContainer.style.border = "solid 3px #000000";
-else bookContainer.style.border = "none";
+if (bookCollection.bookArray[0]) { bookContainer.style.border = 'solid 3px #000000'; } else bookContainer.style.border = 'none';
 
-const addButton = document.querySelector(".add-button");
-const titleInput = document.querySelector("#title-input");
-const authorInput = document.querySelector("#author-input");
-addButton.addEventListener("click", () => {
+const addButton = document.querySelector('.add-button');
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+addButton.addEventListener('click', () => {
   if (titleInput.value && authorInput.value) {
     bookCollection.addBook(titleInput.value, authorInput.value);
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   }
 });
