93e23b60729acdc2e0096bcb4241632a1791cfa9
torobucci <kevintoro254@gmail.com>
2023-02-22 12:58:36 +0300

    update index.js

--- index.js
+++ index.js
@@ -15,22 +15,20 @@ class BookCollection {
     this.bookArray = bookArray;
   }
   removeBook(book) {
-    console.log(this.bookArray);
     this.bookArray.splice(book, 1);
-    console.log(this.bookArray);
-
     localStorage.setItem(
       "bookCollectionArray",
       JSON.stringify(bookCollection.bookArray)
     );
+    bookContainer.innerHTML = "";
     this.bookArray.forEach((book, i) => {
       book.id = i;
+      displayBook(book);
+      document
+        .querySelector(`#remove-${book.id}`)
+        ?.addEventListener(`click`, this.removeBook.bind(this, book));
     });
-    bookContainer.innerHTML = "";
-    this.bookArray.forEach((book) => displayBook(book));
-    document
-      .querySelector(`#remove-${book.id}`)
-      ?.addEventListener(`click`, this.removeBook.bind(this, book));
+
     this.bookArray[0]
       ? (bookContainer.style.border = "solid 3px #000000")
       : (bookContainer.style.border = "none");
