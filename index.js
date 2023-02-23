6fac7e3f1216a16f32a0049a93c78d79db19fe0d
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-02-24 01:48:15 +0200

    Refactor code

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const bookContainer = document.querySelector(".books-container");
-
-function displayBook(book) {
-  bookContainer.insertAdjacentHTML(
-    "beforeend",
-    `<div class="book-details" id="book-${book.id}">
-        <p>"${book.title}" by ${book.author}</p> 
-        <button class="remove" type="button" id="remove-${book.id}">Remove</button>
-      </div>`
-  );
-}
+const bookContainer = document.querySelector('.books-container');
 
 class BookCollection {
   constructor(bookArray) {
     this.bookArray = bookArray;
   }
 
-  removeBook(book) {
-    this.bookArray.splice(book, 1);
-    localStorage.setItem("bookCollectionArray", JSON.stringify(this.bookArray));
-    bookContainer.innerHTML = "";
-    this.bookArray.forEach((book, i) => {
-      book.id = i;
-      displayBook(book);
-      if (document.querySelector(`#remove-${book.id}`)) {
-        document
-          .querySelector(`#remove-${book.id}`)
-          .addEventListener("click", this.removeBook.bind(this, book));
-      }
+  displayBooks() {
+    bookContainer.innerHTML = '';
+    this.bookArray.forEach((book) => {
+      bookContainer.insertAdjacentHTML(
+        'beforeend',
+        `<div class="book-details" id="book-${book.id}">
+        <p>"${book.title}" by ${book.author}</p> 
+        <button class="remove" type="button" id="remove-${book.id}">Remove</button>
+      </div>`,
+      );
     });
 
-    if (this.bookArray[0]) bookContainer.style.border = "solid 3px #000000";
-    else bookContainer.style.border = "none";
+    if (this.bookArray.length !== 0) {
+      bookContainer.style.border = 'solid 3px #000000';
+    } else bookContainer.style.border = 'none';
+  }
+
+  storeInLocalBrowser() {
+    localStorage.setItem('bookCollectionArray', JSON.stringify(this.bookArray));
+  }
+
+  removeBook(book) {
+    this.bookArray.splice(book, 1);
+    this.storeInLocalBrowser();
+    bookContainer.innerHTML = '';
+    this.bookArray.forEach((book, i) => {
+      book.id = i;
+    });
+    this.displayBooks();
   }
 
   addBook(title, author) {
@@ -39,68 +42,50 @@ class BookCollection {
       author,
       id: this.bookArray.length,
     });
-    localStorage.setItem("bookCollectionArray", JSON.stringify(this.bookArray));
-
-    displayBook(this.bookArray[this.bookArray.length - 1]);
-    if (
-      document.querySelector(
-        `#remove-${this.bookArray[this.bookArray.length - 1].id}`
-      )
-    ) {
-      document
-        .querySelector(
-          `#remove-${this.bookArray[this.bookArray.length - 1].id}`
-        )
-        .addEventListener("click", () => {
-          this.removeBook(this.bookArray[this.bookArray.length - 1]);
-        });
-    }
-    if (this.bookArray[0]) bookContainer.style.border = "solid 3px #000000";
-    else bookContainer.style.border = "none";
+    this.storeInLocalBrowser();
+    this.displayBooks();
   }
 }
 
-const bookCollection = localStorage.getItem("bookCollectionArray")
-  ? new BookCollection(JSON.parse(localStorage.getItem("bookCollectionArray")))
+const bookCollection = localStorage.getItem('bookCollectionArray')
+  ? new BookCollection(JSON.parse(localStorage.getItem('bookCollectionArray')))
   : new BookCollection([]);
-bookCollection.bookArray.forEach((book) => {
-  displayBook(book);
-  if (document.querySelector(`#remove-${book.id}`)) {
-    document
-      .querySelector(`#remove-${book.id}`)
-      .addEventListener(
-        "click",
-        bookCollection.removeBook.bind(bookCollection, book)
-      );
-  }
-});
-if (bookCollection.bookArray[0]) {
-  bookContainer.style.border = "solid 3px #000000";
-} else bookContainer.style.border = "none";
 
-const addButton = document.querySelector(".add-button");
-const titleInput = document.querySelector("#title-input");
-const authorInput = document.querySelector("#author-input");
-addButton.addEventListener("click", () => {
+bookCollection.displayBooks();
+
+const addButton = document.querySelector('.add-button');
+const titleInput = document.querySelector('#title-input');
+const authorInput = document.querySelector('#author-input');
+addButton.addEventListener('click', () => {
   if (titleInput.value && authorInput.value) {
     bookCollection.addBook(titleInput.value, authorInput.value);
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   }
 });
 
-document.querySelectorAll(".section").forEach((section) => {
-  if (!(section.id == "book-list")) section.style.display = "none";
+document.querySelectorAll('.section').forEach((section) => {
+  if (!(section.id === 'book-list')) section.style.display = 'none';
 });
 
-const navBar = document.querySelector(".nav-Bar");
-navBar.addEventListener("click", (e) => {
+const navBar = document.querySelector('.nav-Bar');
+navBar.addEventListener('click', (e) => {
   e.preventDefault();
-  if (e.target.classList.contains("nav-link")) {
-    const anchorHref = e.target.href.split("#")[1];
-    document.querySelectorAll(".section").forEach((section) => {
-      if (section.id === anchorHref) section.style.display = "flex";
-      else section.style.display = "none";
+  if (e.target.classList.contains('nav-link')) {
+    const anchorHref = e.target.href.split('#')[1];
+    document.querySelectorAll('.section').forEach((section) => {
+      if (section.id === anchorHref) section.style.display = 'flex';
+      else section.style.display = 'none';
     });
+  }
+});
+
+bookContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const removeBtn = e.target;
+    const book = bookCollection.bookArray.find(
+      (book) => book.id === removeBtn.id.split('-')[1],
+    );
+    bookCollection.removeBook(book);
   }
 });
